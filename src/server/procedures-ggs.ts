f8bcf4c99a41d887498c6bf33cd8f3e41ce983d4
Manuel De la Penna <manueldelapenna@gmail.com>
2022-08-10 18:43:03 -0300

    codigos de blaise en query de encuesta andando bien

--- src/server/procedures-ggs.ts
+++ src/server/procedures-ggs.ts
@@ -7,7 +7,7 @@ import { setHdrQuery } from "dmencu/dist/server/server/procedures-dmencu"
 setHdrQuery((quotedCondViv:string)=>{
     return `
     with viviendas as 
-        (select t.enc, t.json_encuesta as respuestas, t.resumen_estado as "resumenEstado", 
+        (select enc, t.json_encuesta as respuestas, t.resumen_estado as "resumenEstado", 
             jsonb_build_object(
                 'dominio'       , dominio       ,
                 'nomcalle'      , nomcalle      ,
@@ -22,24 +22,22 @@ setHdrQuery((quotedCondViv:string)=>{
                 'prioridad'     , reserva+1     ,
                 'observaciones' , tt.carga_observaciones ,
                 'cita'          , cita ,
-                'carga'         , t.area         
+                'carga'         , t.area
             ) as tem, t.area,
-            --TODO: GENERALIZAR
             jsonb_build_object(
                 'tarea', tarea,
                 'fecha_asignacion', fecha_asignacion,
                 'asignado', asignado,
                 'main_form', main_form
             ) as tarea,
-            min(fecha_asignacion) as fecha_asignacion,
-            jsonb_agg(jsonb_build_object(
-				'enc',th.enc,
-				'hogar',th.hogar, 
-				'idblaise', th.idblaise
-			)) codigos_blaise
-            from tem t left join tareas_tem tt using (operativo, enc) left join tareas using (tarea) left join tem_hogar th on (th.operativo=t.operativo and th.enc =t.enc)
+            ${jsono(
+                `select enc, hogar, idblaise from tem_hogar where operativo = t.operativo and enc = t.enc`,
+                'hogar'
+            )} as "codigos_blaise",
+            min(fecha_asignacion) as fecha_asignacion
+            from tem t left join tareas_tem tt using (operativo, enc) left join tareas using (tarea)
             where ${quotedCondViv}
-            group by t.enc, t.json_encuesta, t.resumen_estado, dominio, nomcalle,sector,edificio, entrada, nrocatastral, piso,departamento,habitacion,casa,reserva,tt.carga_observaciones, cita, t.area, tarea, fecha_asignacion, asignado, main_form
+            group by t.operativo, t.enc, t.json_encuesta, t.resumen_estado, dominio, nomcalle,sector,edificio, entrada, nrocatastral, piso,departamento,habitacion,casa,reserva,tt.carga_observaciones, cita, t.area, tarea, fecha_asignacion, asignado, main_form
         )
         select jsonb_build_object(
                 'viviendas', ${jsono(
@@ -54,7 +52,7 @@ setHdrQuery((quotedCondViv:string)=>{
                     group by area, observaciones_hdr`, 
                 'fecha')} as cargas,
             ${jsono(
-                `select enc, jsonb_build_object('tem', tem, 'tarea', tarea,'codigosBlaise',codigos_blaise) as otras from viviendas`,
+                `select enc, jsonb_build_object('tem', tem, 'tarea', tarea, 'codigosBlaise', codigos_blaise) as otras from viviendas`,
                     'enc',
                     `otras ||'{}'::jsonb`
                 )}
