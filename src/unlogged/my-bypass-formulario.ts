a4c3d0d0e13a6e6d82dbeac51463255c27461d48
GracielaCanessa <gracane@gmail.com>
2022-08-25 14:17:01 -0300

    de acuerdo a solicitud de campo, consideramos rea hasta fin_1=1

--- src/unlogged/my-bypass-formulario.ts
+++ src/unlogged/my-bypass-formulario.ts
@@ -112,7 +112,7 @@ var esRealizada = (respuestas:Respuestas)=>{
                         var resp_fin3_ind = respuestasP['fin_3' as IdVariable ];
                         //console.log ('resulcita ', resp_resulcita_ind);
                         if(( resp_entrea_ind==1 && resp_reams_ind==1 )||( resp_entrea_ind==2 && resp_resulcita_ind==1)){ 
-                            if(resp_fin1_ind==1 && resp_fin3_ind==1){ 
+                            if(resp_fin1_ind==1){ 
                                 reah = 1    // determinar si esta ok tmb considerar resp_fin3_ind==1 para indicar que es una encuesta respondente
                             }else{
                                 reah=2;
