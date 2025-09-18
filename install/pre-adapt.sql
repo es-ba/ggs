21756f2534720392fabcae07a1db64046207228b
Eugenio <eugenio.arosteguy@gmail.com>
2025-09-18 11:33:15 -0300

    se agregan las tablas_datos para cada tabla de datos que define la app final

---
+++ install/pre-adapt.sql
@@ -0,0 +1,10 @@
+
+select sql2tabla_datos('base', 'tem_hogar', 'GGS_2025');
+select sql2tabla_datos('base', 'tem', 'GGS_2025');
+select sql2tabla_datos('base', 'viviendas', 'GGS_2025');
+select sql2tabla_datos('base', 'visitas', 'GGS_2025');
+select sql2tabla_datos('base', 'hogares', 'GGS_2025');
+select sql2tabla_datos('base', 'personas', 'GGS_2025');
+select sql2tabla_datos('base', 'visitas_sup', 'GGS_2025');
+select sql2tabla_datos('base', 'hogares_sup', 'GGS_2025');
+select sql2tabla_datos('base', 'personas_sup', 'GGS_2025');
