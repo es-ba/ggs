7693bdc2447582124a7980a1957c9268a1c8c2ab
Eugenio <eugenio.arosteguy@gmail.com>
2025-10-21 22:23:01 -0300

    dejamos tipo bigint como en todos lados

--- src/server/table-visitas_web.ts
+++ src/server/table-visitas_web.ts
@@ -9,12 +9,12 @@ export function visitas_web(context: TableContext): TableDefinition {
         "fields": [
             { "name": "operativo", "typeName": "text", "nullable": false },
             { "name": "vivienda", "typeName": "text", "nullable": false },
-            { "name": "visita_web", "typeName": "integer", "nullable": false },
+            { "name": "visita_web", "typeName": "bigint", "nullable": false },
             { "name": "anoenc", "typeName": "bigint", "nullable": true },
             { "name": "rol", "typeName": "text", "nullable": true },
-            { "name": "per", "typeName": "integer", "nullable": true },
+            { "name": "per", "typeName": "bigint", "nullable": true },
             { "name": "usu", "typeName": "text", "nullable": true },
-            { "name": "modo", "typeName": "integer", "nullable": true },
+            { "name": "modo", "typeName": "bigint", "nullable": true },
             { "name": "fecha", "typeName": "text", "nullable": true },
             { "name": "hora", "typeName": "text", "nullable": true },
             { "name": "anotacion", "typeName": "text", "nullable": true }
--- src/server/table-viviendas.ts
+++ src/server/table-viviendas.ts
@@ -1,5 +1,5 @@
 "use strict";
-                
+     
 import {TableDefinition, TableContext} from "./types-ggs";
 export function viviendas(context:TableContext):TableDefinition {
     var esEditable = context.user.rol==='admin';
@@ -7,257 +7,56 @@ export function viviendas(context:TableContext):TableDefinition {
     "name": "viviendas",
     editable: esEditable,    
     "fields": [
-        {
-            "name": "operativo",
-            "typeName": "text",
-            "visible": false,
-            "nullable": false
-        },
-        {
-            "name": "vivienda",
-            "typeName": "text",
-            "nullable": false
-        },
-        {
-            "name": "vdominio",
-            "typeName": "bigint",
-            "visible": false,
-            "nullable": true
-        },
-        {
-            "name": "obs_re",
-            "typeName": "text",
-            "nullable": true
-        },
-        {
-            "name": "total_vis",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "soporte",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "entreav",
-            "typeName": "bigint",
-            "visible": false,
-            "nullable": true
-        },
-        {
-            "name": "identif",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "habita",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "construc",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "razon_viv",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "razon2_2",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "razon2_6",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "razon3",
-            "typeName": "text",
-            "nullable": true
-        },
-        {
-            "name": "resid_hog",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "razon_hog",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "razon2_1",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "razon2_3",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "razon2_5",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "razon_9v",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "contacto",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "v1",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "total_h",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "vdominio_sup",
-            "typeName": "bigint",
-            "visible": false,
-            "nullable": true
-        },
-        {
-            "name": "s1a1_obs_sup",
-            "typeName": "text",
-            "nullable": true
-        },
-        {
-            "name": "datos_personal_sup",
-            "typeName": "text",
-            "nullable": true
-        },
-        {
-            "name": "datos_personal_enc",
-            "typeName": "text",
-            "nullable": true
-        },
-        {
-            "name": "total_vis_sup",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "soporte_sup",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "modo_sup",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "confir_tel_sup",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "domicilio_sup",
-            "typeName": "text",
-            "nullable": true
-        },
-        {
-            "name": "confir_dom_sup",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "sp1a",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "habita_sup",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "construccion_sup",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "razon_viv_sup",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "razon_2_sup",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "razon2_6_sup",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "razon3_sup",
-            "typeName": "text",
-            "nullable": true
-        },
-        {
-            "name": "sp1b",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "razon_hog_sup",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "razon_1_sup",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "razon2_3_sup",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "razon2_5_sup",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "razon_9v_sup",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "sp1c",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "sp3_sup",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        {
-            "name": "total_h_sup",
-            "typeName": "bigint",
-            "nullable": true
-        },
-        { name: "consistido"    , label:'consistido'            , typeName: 'timestamp'},
-
+        {"name": "operativo", "typeName": "text", "visible": false, "nullable": false},
+        {"name": "vivienda", "typeName": "text", "nullable": false},
+        {"name": "vdominio", "typeName": "bigint", "visible": false, "nullable": true},
+        {"name": "obs_re", "typeName": "text", "nullable": true},
+        {"name": "total_vis", "typeName": "bigint", "nullable": true},
+        {"name": "soporte", "typeName": "bigint", "nullable": true},
+        {"name": "entreav", "typeName": "bigint", "visible": false, "nullable": true},
+        {"name": "identif", "typeName": "bigint", "nullable": true},
+        {"name": "habita", "typeName": "bigint", "nullable": true},
+        {"name": "construc", "typeName": "bigint", "nullable": true},
+        {"name": "razon_viv", "typeName": "bigint", "nullable": true},
+        {"name": "razon2_2", "typeName": "bigint", "nullable": true},
+        {"name": "razon2_6", "typeName": "bigint", "nullable": true},
+        {"name": "razon3", "typeName": "text", "nullable": true},
+        {"name": "resid_hog", "typeName": "bigint", "nullable": true},
+        {"name": "razon_hog", "typeName": "bigint", "nullable": true},
+        {"name": "razon2_1", "typeName": "bigint", "nullable": true},
+        {"name": "razon2_3", "typeName": "bigint", "nullable": true},
+        {"name": "razon2_5", "typeName": "bigint", "nullable": true},
+        {"name": "razon_9v", "typeName": "bigint", "nullable": true},
+        {"name": "contacto", "typeName": "bigint", "nullable": true},
+        {"name": "v1", "typeName": "bigint", "nullable": true},
+        {"name": "total_h", "typeName": "bigint", "nullable": true},
+        {"name": "vdominio_sup", "typeName": "bigint", "visible": false, "nullable": true},
+        {"name": "s1a1_obs_sup", "typeName": "text", "nullable": true},
+        {"name": "datos_personal_sup", "typeName": "text", "nullable": true},
+        {"name": "datos_personal_enc", "typeName": "text", "nullable": true},
+        {"name": "total_vis_sup", "typeName": "bigint", "nullable": true},
+        {"name": "soporte_sup", "typeName": "bigint", "nullable": true},
+        {"name": "modo_sup", "typeName": "bigint", "nullable": true},
+        {"name": "confir_tel_sup", "typeName": "bigint", "nullable": true},
+        {"name": "domicilio_sup", "typeName": "text", "nullable": true},
+        {"name": "confir_dom_sup", "typeName": "bigint", "nullable": true},
+        {"name": "sp1a", "typeName": "bigint", "nullable": true},
+        {"name": "habita_sup", "typeName": "bigint", "nullable": true},
+        {"name": "construccion_sup", "typeName": "bigint", "nullable": true},
+        {"name": "razon_viv_sup", "typeName": "bigint", "nullable": true},
+        {"name": "razon_2_sup", "typeName": "bigint", "nullable": true},
+        {"name": "razon2_6_sup", "typeName": "bigint", "nullable": true},
+        {"name": "razon3_sup", "typeName": "text", "nullable": true},
+        {"name": "sp1b", "typeName": "bigint", "nullable": true},
+        {"name": "razon_hog_sup", "typeName": "bigint", "nullable": true},
+        {"name": "razon_1_sup", "typeName": "bigint", "nullable": true},
+        {"name": "razon2_3_sup", "typeName": "bigint", "nullable": true},
+        {"name": "razon2_5_sup", "typeName": "bigint", "nullable": true},
+        {"name": "razon_9v_sup", "typeName": "bigint", "nullable": true},
+        {"name": "sp1c", "typeName": "bigint", "nullable": true},
+        {"name": "sp3_sup", "typeName": "bigint", "nullable": true},
+        {"name": "total_h_sup", "typeName": "bigint", "nullable": true},
+        {"name": "consistido"    , "label":'consistido' , "typeName": 'timestamp'}
     ],
     "sql": {
         "isReferable": true
