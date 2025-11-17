4634bc1353ff099f59f2dd4fa5ba1339ef0da505
Eugenio <eugenio.arosteguy@gmail.com>
2025-11-17 15:12:15 -0300

    subimos versión DM

--- src/server/app-ggs.ts
+++ src/server/app-ggs.ts
@@ -18,7 +18,7 @@ import { hogares_sup         } from './table-hogares_sup';
 import { personas_sup        } from './table-personas_sup';
 import { grilla_rea_sin_blaise } from './grilla_rea_sin_blaise';
 
-const APP_DM_VERSION="#25-10-45";
+const APP_DM_VERSION="#25-11-17";
 
 export type Constructor<T> = new(...args: any[]) => T;
 export function emergeAppGgs<T extends Constructor<dmencu.AppAppDmEncuType>>(Base:T){
