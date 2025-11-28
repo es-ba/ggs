3ef1579b694e162943f5d3e0c15da0089b205036
Eugenio <eugenio.arosteguy@gmail.com>
2025-11-28 16:03:05 -0300

    reorganización archivos

--- install/pre-adapt.sql
+++ install/pre-adapt.sql
@@ -2,12 +2,13 @@
 select sql2tabla_datos('base', 'tem_blaise', 'GGS_2025');
 select sql2tabla_datos('base', 'tem', 'GGS_2025');
 select sql2tabla_datos('base', 'viviendas', 'GGS_2025');
-select sql2tabla_datos('base', 'visitas', 'GGS_2025');
-select sql2tabla_datos('base', 'hogares', 'GGS_2025');
-select sql2tabla_datos('base', 'personas', 'GGS_2025');
 select sql2tabla_datos('base', 'visitas_sup', 'GGS_2025');
-select sql2tabla_datos('base', 'hogares_sup', 'GGS_2025');
-select sql2tabla_datos('base', 'personas_sup', 'GGS_2025');
 select sql2tabla_datos('base', 'visitas_web', 'GGS_2025');
 select sql2tabla_datos('base', 'visitas_tel', 'GGS_2025');
 select sql2tabla_datos('base', 'visitas_pres', 'GGS_2025');
+
+-- select sql2tabla_datos('base', 'visitas', 'GGS_2025');
+-- select sql2tabla_datos('base', 'hogares', 'GGS_2025');
+-- select sql2tabla_datos('base', 'hogares_sup', 'GGS_2025');
+-- select sql2tabla_datos('base', 'personas', 'GGS_2025');
+-- select sql2tabla_datos('base', 'personas_sup', 'GGS_2025');
